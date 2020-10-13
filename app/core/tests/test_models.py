09c86629941ca7073fb3ac4bf02656cbcf0817aa
Paras <helloiamparas@gmail.com>
2020-10-13 14:57:00 +0100

    resolved travis ci issue

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
+
     def test_create_user_with_email_successful(self):
         email = "admim@gamil.com"
         password = "123admin123"
