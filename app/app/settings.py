5e1dbcbd6b84fe9094bde16c668dbcfefe9d40de
Paras <helloiamparas@gmail.com>
2020-11-01 15:05:16 +0000

    added image field recipe model

--- app/app/settings.py
+++ app/app/settings.py
@@ -122,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
